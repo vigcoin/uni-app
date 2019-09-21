88f19babbcf8fe5880fe7b6552ad8dcd09882365
calidion <calidion@gmail.com>
2019-09-21 22:56:34 +0800

    init

---
+++ src/pages/index/index.ts
@@ -0,0 +1,14 @@
+import Vue from 'vue';
+export default Vue.extend({
+  data() {
+    return {
+      title: 'Hello'
+    }
+  },
+  onLoad() {
+
+  },
+  methods: {
+
+  }
+});
---
+++ src/pages/index/index.vue
@@ -0,0 +1,8 @@
+<template src="./index.html">
+</template>
+
+<script lang="ts" src="./index.ts">
+</script>
+
+<style lang="scss" src="./index.scss">
+</style>
